1c6001b73aaf127916d7ae386d8758ed90cf03d2
famesensor <sathit.ju@outlook.com>
2021-04-30 00:39:49 +0700

    [:hammer:]: handler function upload file to server

--- client/src/App.js
+++ client/src/App.js
@@ -1,113 +1,83 @@
-import React, { Component } from "react";
-import axios from "axios";
-import "./App.css";
-import { storage } from "./firebase-config";
+import React, { useState } from 'react';
+import 'antd/dist/antd.css';
+import './App.css';
+import { Row, Col, Upload, Button, message } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
+import axios from 'axios';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedFile: null,
-      firebaseImg: null
+function App() {
+    const [filelist, setFile] = useState([]);
+    const [upload, setUpload] = useState(false);
+
+    const beforeUpload = (file) => {
+        setFile((filelist) => [...filelist, file]);
+        return false;
     };
-  }
 
-  // onChangeHandler = event => {
-  //   this.setState({
-  //     selectedFile: event.target.files[0],
-  //     loaded: 0
-  //   });
-  //   console.log(event.target.files[0]);
-  // };
+    const submit = async (e) => {
+        setUpload(true);
 
-  // onClickHandler = () => {
-  //   const data = new FormData();
-  //   data.append("imageName", "1")
-  //   data.append("file", this.state.selectedFile);
-  //   axios
-  //     .post("http://localhost:8000/upload", data, {
-  //       // receive two    parameter endpoint url ,form data
-  //     })
-  //     .then(res => {
-  //       // then print response status
-  //       console.log(res.statusText);
-  //     });
-  // };
+        const data = new FormData();
+        filelist.forEach((file) => {
+            data.append('files', file, file.name);
+        });
+        data.append('name', 'famesensor');
+        try {
+            let res = await axios.post(
+                'http://localhost:5000/api/image/upload-local-storage',
+                data
+            );
+            message.success(res.data.data);
+            setUpload(false);
+            setFile([]);
+        } catch (error) {
+            message.error(error);
+            setUpload(false);
+            setFile([]);
+        }
+    };
 
-  setDefaultimage(type) {
-    if (type === 'firebase') {
-      this.setState({
-        firebaseImg: null
-      })
-    }
-  }
+    const onRemove = (file) => {
+        const index = filelist.indexOf(file);
+        const newFileList = filelist.slice();
+        newFileList.splice(index, 1);
+        setFile(newFileList);
+    };
 
-  onChangeHandler = e => {
-    this.setState({
-      firebaseImg: URL.createObjectURL(e.target.files[0])
-    });
-  }
-
-  upload(e, type) {
-    let imageObj = {};
-
-    let currentImageName = "firebase-image-" + Date.now();
-
-    let uploadImage = storage.ref(`images/${currentImageName}`).put(e.target.files[0]);
-    
-    uploadImage.on('state_changed',
-      (snapshot) => { },
-      (error) => {
-        alert(error);
-      },
-      () => {
-        storage.ref('images').child(currentImageName).getDownloadURL()
-          .then(url => {
-            this.setState({
-              firebaseImg: url
-            })
-
-            imageObj = {
-              imageName: currentImageName,
-              imageData: url
-            }
-            console.log(imageObj);
-            
-            axios.post('/api/image/uploadbase', imageObj)
-              .then((data) => {
-                if (data.success) {
-                  alert("Yes!!")
-                }
-              })  
-              .catch((err) => {
-                alert("Say no");
-                this.setDefaultimage("firebase");
-              })
-          })
-      }
-    )
-
-  }
-
-  render() {
     return (
-      <div className="container">
-        <div className="row">
-          {/* <input type="file" name="file" onChange={this.onChangeHandler} />
-          <button
-            type="button"
-            className="btn btn-success btn-block"
-            onClick={this.onClickHandler}
-          >
-            Upload
-          </button> */}
-          <input type="file" onChange={ this.onChangeHandler } />
-          <img src={ this.state.firebaseImg } />
-          <button onClick={ (e) => this.upload(e, "firebase") }>Upload</button>
-        </div>
-      </div>
+        <>
+            <div className='App'>
+                <div>
+                    <Row gutter={16}>
+                        <Col span={12}>
+                            <Upload
+                                beforeUpload={beforeUpload}
+                                onRemove={onRemove}
+                                multiple
+                            >
+                                <Button icon={<UploadOutlined />}>
+                                    Upload File
+                                </Button>
+                            </Upload>
+                            <p></p>
+                            <Button
+                                onClick={submit}
+                                disabled={filelist.length === 0}
+                                loading={upload}
+                            >
+                                {upload ? 'Uploading' : 'Start Upload'}
+                            </Button>
+                        </Col>
+                        <Col span={12}>
+                            <Button icon={<UploadOutlined />}>
+                                Click to Upload
+                            </Button>
+                        </Col>
+                    </Row>
+                </div>
+            </div>
+        </>
     );
-  }
 }
 
 export default App;
