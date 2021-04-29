d14ff639535585926084fc4fbe24b337c753d004
famesensor <sathit.ju@outlook.com>
2021-04-30 00:44:10 +0700

    [:hammer:]: setup route upload local storage

--- router/v1/image.js
+++ router/v1/image.js
@@ -1,27 +1,37 @@
 const express = require('express');
 const router = express.Router();
-const multer = require('multer');
+const { upload } = require('../../utils/multer');
 
 const Image = require('../../models/image');
 
-router.post('/uploadbase', (req, res) => {
-    // console.log(req.body)
+router.post('/uploadbase', async (req, res, next) => {
     const newImage = new Image({
         imageName: req.body.imageName,
         imageData: req.body.imageData
     });
 
-    newImage.save()
-        .then((res) => {
-            // console.log(res)
-            res.status(200).json({
-                success: true,
-                data: res
-            })
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+    try {
+        let res_ = await newImage.save();
+
+        res.status(200).json({
+            success: true,
+            data: res_
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: `Internal server error.`
+        });
+    }
 });
 
-module.exports = router; 
+router
+    .route('/upload-local-storage')
+    .post(upload.array('files', 5), async (req, res, next) => {
+        res.json({
+            success: true,
+            data: 'upload file success'
+        });
+    });
+
+module.exports = router;
